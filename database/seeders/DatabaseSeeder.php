5cf881a7c5be20d689fb6c449c2f8f143d27a09b
Huzaifa Imran <104152779+Huzaifa709@users.noreply.github.com>
2024-05-15 09:19:25 -0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,19 +4,30 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
-        // \App\Models\User::factory(10)->create();
+    // public function run(): void
+    // {
+    //     // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+    //     // \App\Models\User::factory()->create([
+    //     //     'name' => 'Test User',
+    //     //     'email' => 'test@example.com',
+    //     // ]);
+    // }
+
+    public function run()
+    {
+        // Update existing users' user_type to 'user'
+        User::whereNull('user_type')->update(['user_type' => 'user']);
     }
+
+
+
 }
+
