ea4b7258eaba2cdc754b1f24ca223a4c53460077
Huzaifa Imran <104152779+Huzaifa709@users.noreply.github.com>
2024-05-15 08:54:04 -0700

    Update RegisteredUserController.php

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,12 +33,14 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+           // 'user_type' => ['required', 'string', 'max:255'],
+             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+           // 'user_type' => ['required', 'string', 'max:255'],
             'password' => Hash::make($request->password),
         ]);
 
